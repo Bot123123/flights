a0f944500b6dd466a9079a4b8a0a23778a5e61a7
Andrew Galenko <andrew@galenko.org>
2016-11-23 23:15:56 +0200

    Add SkyScanner class, get_the_cheapest_price method and example of usage

--- skyscanner_consts.py
+++ skyscanner_consts.py
@@ -5,3 +5,11 @@ API_URL = 'http://partners.api.skyscanner.net/apiservices'
 class URL:
     MIN_PRICE = '{api_url}/browsequotes/v1.0/UA/UAH/en-GB/{{airport_from}}/{{airport_to}}/{{date}}?apiKey={key}'\
         .format(api_url=API_URL, key=API_KEY)
+
+
+class AIRPORT:
+    UA_KYIV = 'KBP'
+    UA_LVIV = 'LWO'
+
+
+
--- skyscanner_wrapper.py
+++ skyscanner_wrapper.py
@@ -1,10 +1,27 @@
-from skyscanner_consts import URL
+from skyscanner_consts import URL, AIRPORT
 from time import time
 import requests
 
-arrival_date = '2016-11-28'
 
-resp = requests.get(URL.MIN_PRICE.format(date=arrival_date, airport_from='KBP', airport_to='LWO')).json()
-resp['response_time'] = time()
+class SkyScanner:
 
-print resp
+    @staticmethod
+    def get_the_cheapest_price(ap_from, ap_to, arrival_date):
+        """
+
+        :param ap_from: <sting> aiport id from consts.AIRPORT
+        :param ap_to: <sting> aiport id from consts.AIRPORT
+        :arrival_date: <string> date in format 'YYYY-MM-DD'
+        :return:
+        """
+        resp = requests.get(URL.MIN_PRICE.format(date=arrival_date, airport_from=ap_from, airport_to=ap_to)).json()
+        resp['response_time'] = time()
+        return resp
+
+
+if __name__ == '__main__':
+    arrival_date = '2016-11-28'
+    ap_from = AIRPORT.UA_KYIV
+    ap_to = AIRPORT.UA_LVIV
+
+    print SkyScanner.get_the_cheapest_price(ap_from, ap_to, arrival_date)
